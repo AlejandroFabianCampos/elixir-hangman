0c7bcfa8ebbc0e5ddcb8172624806543a036bb7c
Alejandro Fabian Campos <camposalejandrofabian@gmail.com>
2021-06-23 21:49:50 -0300

    feat(Hangman.Game): added the ability to score good guesses, progress and win the game

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -6,10 +6,14 @@ defmodule Hangman.Game do
     used: MapSet.new()
   )
 
-  def new_game() do
+  def new_game(word) do
     %Hangman.Game{
-      letters: Dictionary.random_word() |> String.codepoints()
+      letters: word |> String.codepoints()
     }
+  end
+
+  def new_game() do
+    new_game(Dictionary.random_word)
   end
 
   def make_move(game = %Hangman.Game{game_state: state}, _guess) when state in [:won, :lost],
@@ -20,16 +24,32 @@ defmodule Hangman.Game do
   end
 
   def accept_move(game, _guess, _allready_guessed = true) do
-    game = Map.put(game, :game_state, :already_used)
-    { game, tally(game) }
+    { Map.put(game, :game_state, :already_used), tally(game) }
   end
 
   def accept_move(game, guess, _allready_guessed) do
-    game = Map.put(game, :used, MapSet.put(game.used, guess))
-    { game, tally(game) }
+    {
+      Map.put(game, :used, MapSet.put(game.used, guess))
+      |> score_guess(Enum.member?(game.letters, guess)),
+      tally(game)
+    }
+  end
+
+  def score_guess(game, _good_guess = true) do
+    new_state = MapSet.new(game.letters)
+    |> MapSet.subset?(game.used)
+    |> maybe_won()
+    Map.put(game, :game_state, new_state)
+  end
+
+  def score_guess(game, _not_good_guess) do
+    game
   end
 
   def tally(_game) do
     :placeholder
   end
+
+  def maybe_won(true), do: :won
+  def maybe_won(_), do: :good_guess
 end
